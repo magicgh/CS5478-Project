a77897774c87ee5a5e8f465921827d468b088d18
Guiltylife <59986490+Guiltylife@users.noreply.github.com>
2023-11-15 22:46:02 +0800

    Update panda_sim_grasp.py

--- Grasping/panda_sim_grasp.py
+++ Grasping/panda_sim_grasp.py
@@ -51,12 +51,12 @@ class PandaSimAuto(object):
                                                       np.array([0.1, 0.3, -0.5]) + self.offset,
                                                       flags=flags))
         # self.bullet_client.changeVisualShape(self.legos[0], -1, rgbaColor=[1, 0, 0, 1])  # 修改目标物体的颜色
-        self.legos.append(self.bullet_client.loadURDF("lego/lego.urdf",
-                                                      np.array([-0.1, 0.3, -0.5]) + self.offset,
-                                                      flags=flags))
-        self.legos.append(self.bullet_client.loadURDF("lego/lego.urdf",
-                                                      np.array([0.1, 0.3, -0.7]) + self.offset,
-                                                      flags=flags))
+        # self.legos.append(self.bullet_client.loadURDF("lego/lego.urdf",
+        #                                               np.array([-0.1, 0.3, -0.5]) + self.offset,
+        #                                               flags=flags))
+        # self.legos.append(self.bullet_client.loadURDF("lego/lego.urdf",
+        #                                               np.array([0.1, 0.3, -0.7]) + self.offset,
+        #                                               flags=flags))
 
         # self.sphereId = self.bullet_client.loadURDF("sphere_small.urdf",
         #                                             np.array([0, 0.3, -0.6]) + self.offset,
@@ -109,9 +109,9 @@ class PandaSimAuto(object):
 
         self.state_t = 0
         self.cur_state = 0
-        # 0：空闲、1：移动到托盘中心上方、2：获取深度图片计算最优抓取位置、3：移动到目标上方、4：张开机械手、5：移动到目标处、6：闭合机械手、3：移动到目标上方、7：移动到结束位置
-        self.states = [0, 1, 2, 3, 4, 5, 6, 3, 7, 4]
-        self.state_durations = [1, 2, 0, 2, 1, 4, 1, 2, 2, 1]
+        # 0：初始状态、1：移动到空闲位置、2：获取深度图片计算最优抓取位置、3：张开机械手、4：移动到目标处、5：闭合机械手、6：移动到目标位置
+        self.states = [0, 1, 2, 3, 4, 5, 6, 2]
+        self.state_durations = [1, 1, 0, 1, 2, 1, 2, 1]
 
         current_path = os.path.dirname(os.path.abspath(__file__))
         config = YamlConfig(os.path.join(current_path, 'gqcnn_pj.yaml'))
@@ -134,7 +134,7 @@ class PandaSimAuto(object):
         if self.state_t > self.state_durations[self.cur_state]:
             self.cur_state += 1
             if self.cur_state >= len(self.states):
-                self.cur_state = 0
+                self.cur_state = 1
             self.state_t = 0
             self.path = list()
             self.path_idx = 0
@@ -147,26 +147,22 @@ class PandaSimAuto(object):
 
         if self.state == 1:
             if len(self.path) == 0:
-                self.path = self.get_path([0, 0.4, -0.6], [math.pi / 2., 0, 0.])
-        elif self.state == 2:
+                self.path = self.get_path([0.6, 0.4, 0], [math.pi / 2., 0, 0.])
+        if self.state == 2:
             self.graspPos = self.get_grasp_pos()
         elif self.state == 3:
-            if len(self.path) == 0:
-                self.path = self.get_path([self.graspPos[0], 0.4, self.graspPos[2]], [math.pi / 2., 0., 0.])
-        elif self.state == 4:  # 张开机械手
             self.finger_target = 0.04
-        elif self.state == 5:
-            # gripper_height = 0.034
+        elif self.state == 4:
             if len(self.path) == 0:
                 self.path = self.get_path([self.graspPos[0], self.graspPos[1], self.graspPos[2]],
                                           [math.pi / 2., self.graspPos[3], 0.])
-        elif self.state == 6:  # 闭合机械手
+        elif self.state == 5:  # 张开机械手
             self.finger_target = 0.01
-        elif self.state == 7:
+        elif self.state == 6:
             if len(self.path) == 0:
-                self.path = self.get_path([0.6, 0.4, 0], [math.pi / 2., 0., 0.])
+                self.path = self.get_path([0, 0.4, -0.6], [math.pi / 2., 0., 0.])
 
-        if self.state == 1 or self.state == 3 or self.state == 5 or self.state == 7:
+        if self.state == 1 or self.state == 4 or self.state == 6:
             for i in range(9):
                 self.bullet_client.setJointMotorControl2(self.panda, i, self.bullet_client.POSITION_CONTROL,
                                                          self.path[self.path_idx][i], force=5 * 240.)
@@ -174,50 +170,10 @@ class PandaSimAuto(object):
             if self.path_idx < len(self.path) - 1:
                 self.path_idx += 1
 
-        if self.state == 4 or self.state == 6:
+        if self.state == 3 or self.state == 5:
             for i in [9, 10]:
                 self.bullet_client.setJointMotorControl2(self.panda, i, self.bullet_client.POSITION_CONTROL,
                                                          self.finger_target, force=10)
-
-    # def step(self):
-    #     self.bullet_client.submitProfileTiming("step")
-    #     self.update_state()
-    #
-    #     pos = self.prev_pos
-    #     orn = [math.pi / 2., 0, 0.]  # 需要根据预测结果调整第二个维度
-    #     alpha = 0.01  # 移动速度，越接近1越慢
-    #     if self.state == 1:
-    #         pos = self.get_next_pos(0, 0.4, -0.6, alpha)
-    #     elif self.state == 2:
-    #         self.graspPos = self.get_grasp_pos()
-    #     elif self.state == 3:
-    #         pos = self.get_next_pos(self.graspPos[0], 0.4, self.graspPos[2], alpha)
-    #         orn = self.get_next_orn(math.pi / 2., 0., 0., alpha)
-    #     elif self.state == 4:  # 张开机械手
-    #         self.finger_target = 0.04
-    #     elif self.state == 5:
-    #         # gripper_height = 0.034
-    #         pos = self.get_next_pos(self.graspPos[0], self.graspPos[1] + 0.034, self.graspPos[2], alpha)
-    #         orn = self.get_next_orn(math.pi / 2., self.graspPos[3], 0., alpha)
-    #     elif self.state == 6:  # 闭合机械手
-    #         self.finger_target = 0.01
-    #     elif self.state == 7:
-    #         pos = self.get_next_pos(0.6, 0.4, 0, alpha)
-    #
-    #     if pos != self.prev_pos:
-    #         self.prev_pos = pos
-    #         self.prev_orn = orn
-    #         quaternion_orn = self.bullet_client.getQuaternionFromEuler(orn)
-    #         jointPoses = self.bullet_client.calculateInverseKinematics(self.panda, pandaEndEffectorIndex, pos,
-    #                                                                    quaternion_orn, ll, ul, jr, rp,
-    #                                                                    maxNumIterations=20)
-    #         for i in range(pandaNumDofs):
-    #             self.bullet_client.setJointMotorControl2(self.panda, i, self.bullet_client.POSITION_CONTROL,
-    #                                                      jointPoses[i], force=5 * 240.)
-    #
-    #     for i in [9, 10]:
-    #         self.bullet_client.setJointMotorControl2(self.panda, i, self.bullet_client.POSITION_CONTROL,
-    #                                                  self.finger_target, force=10)
 
     def get_path(self, des_pos, des_orn, algorithm='prm'):
         current_joint_state = [self.bullet_client.getJointState(self.panda, i)[0] for i in range(9)]
@@ -249,10 +205,10 @@ class PandaSimAuto(object):
         fov = 49  # 相机视角
         aspect = width / height  # 宽高比
         near = 0.01  # 最近拍摄距离
-        far = 1.0  # 最远拍摄距离
+        far = 2.0  # 最远拍摄距离
 
         # cameraPos = self.prev_pos  # 相机位置
-        cameraPos = [-0.5, 0.5, -0.6]
+        cameraPos = [0, 0.6, -0.6]
         targetPos = [0, 0, -0.6]  # 目标位置，与相机位置之间的向量构成相机朝向
         cameraUpPos = [1, 0, 0]  # 相机顶端朝向
 
@@ -305,20 +261,6 @@ class PandaSimAuto(object):
         angle = (math.pi - action.grasp.angle % math.pi) % math.pi
         if angle > math.pi / 2:
             angle = angle - math.pi
-        # return [cameraPos[0] - pose[0], cameraPos[1] - pose[2], cameraPos[2] + pose[1], angle]
-        # return [pose[0], 0.00, -0.6 + pose[1], angle]
-        return [-cameraPos[0] + pose_world[0], 0.034, cameraPos[2] + pose_world[2], angle]
-
-    def get_next_pos(self, x, y, z, alpha):
-        pos = [0, 0, 0]
-        pos[0] = self.prev_pos[0] * (1 - alpha) + alpha * x
-        pos[1] = self.prev_pos[1] * (1 - alpha) + alpha * y
-        pos[2] = self.prev_pos[2] * (1 - alpha) + alpha * z
-        return pos
-
-    def get_next_orn(self, x, y, z, alpha):
-        orn = [0, 0, 0]
-        orn[0] = self.prev_orn[0] * (1 - alpha) + alpha * x
-        orn[1] = self.prev_orn[1] * (1 - alpha) + alpha * y
-        orn[2] = self.prev_orn[2] * (1 - alpha) + alpha * z
-        return orn
+        return [cameraPos[0] - pose[1], cameraPos[1] - pose[2], cameraPos[2] + pose[0], angle]
+        # return [-pose_world[0], pose_world[2], pose_world[1], angle]
+        # return [cameraPos[0] + pose_world[0], cameraPos[1] + pose_world[1], cameraPos[2] + pose_world[2], angle]
